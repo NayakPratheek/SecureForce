81a706bbb091e9459cb104460944b53b96fdfd7d
Pratheek Nayak B K <128373632+NayakPratheek@users.noreply.github.com>
2024-03-01 20:19:00 +0530

    insertion and retrieval

--- index.js
+++ index.js
@@ -2,25 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./server/db");
-const { insertUnitQ, insertSoldierQ } = require("./q");
+const { insertUnitQ, insertSoldierQ, insertEquipmentQ, insertCommanderQ } = require("./q");
 
 app.use(cors());
 app.use(express.json());
 
 // INSERTING
-app.post("/soldier", async (req, res) => {
-    try {
-        const { soldier_id, soldier_name, soldier_rank, dob, gender, unit_id, date_of_enlistment, date_of_discharge, Regiment_Name, phone, blood_type, physical_fitness_scores } = req.body;
-        const newSoldier = await pool.query(
-            insertSoldierQ,
-            [soldier_id, soldier_name, soldier_rank, dob, gender, unit_id, date_of_enlistment, date_of_discharge, Regiment_Name, phone, blood_type, physical_fitness_scores]
-        );
-        res.json(newSoldier.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
-    }
-});
 
 app.post("/unit", async (req, res) => {
     try {
@@ -29,13 +16,76 @@ app.post("/unit", async (req, res) => {
             insertUnitQ,
             [Unit_ID, Unit_Name, Unit_Type, Unit_Head_Name, Unit_Head_Rank, Unit_Head_Phone, Unit_Location, Date_of_Establishment]
         );
-        res.json(newUnit.rows[0]);
-    }
-    catch (err) {
+        res.send(newUnit.rows[0]);
+    } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error");
     }
-})
+});
+
+app.post("/soldier", async (req, res) => {
+    try {
+        const { Soldier_ID, Soldier_Name, Soldier_Rank, DOB, Gender, Unit_ID, Date_of_Enlistment, Date_of_Discharge, Regiment_Name, Phone, Blood_Type, Physical_Fitness_Scores } = req.body;
+        const newSoldier = await pool.query(
+            insertSoldierQ,
+            [Soldier_ID, Soldier_Name, Soldier_Rank, DOB, Gender, Unit_ID, Date_of_Enlistment, Date_of_Discharge, Regiment_Name, Phone, Blood_Type, Physical_Fitness_Scores]
+        );
+        res.json(newSoldier.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.post("/equipment", async (req, res) => {
+    try {
+        const { Equipment_ID, Equipment_Name, Equipment_Type, Unit_ID, Quantity, Condition } = req.body;
+        const newEquipment = await pool.query(
+            insertEquipmentQ,
+            [Equipment_ID, Equipment_Name, Equipment_Type, Unit_ID, Quantity, Condition]
+        );
+        res.json(newEquipment.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.post("/commander", async (req, res) => {
+    try {
+        const { Commander_ID, Commander_Name, Commander_Rank, Unit_ID, Phone, Date_of_Commission, Awards } = req.body;
+        const newCommander = await pool.query(
+            insertCommanderQ,
+            [Commander_ID, Commander_Name, Commander_Rank, Unit_ID, Phone, Date_of_Commission, Awards]
+        );
+        res.json(newCommander.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// RETRIEVE THE DATA
+
+app.get("/soldiers", async (req, res) => {
+    try {
+        const allSoldiers = await pool.query("SELECT * FROM soldier");
+        res.json(allSoldiers.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.get("/equipments", async (req, res) => {
+    try {
+        const allEquipments = await pool.query("SELECT * FROM equipment");
+        res.json(allEquipments.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 app.listen(5000, () => {
     console.log("Server running at port 5000");
--- q.js
+++ q.js
@@ -1,6 +1,11 @@
 const insertUnitQ = "INSERT INTO unit (Unit_ID, Unit_Name, Unit_Type,Unit_Head_Name,Unit_Head_Rank,Unit_Head_Phone, Unit_Location,Date_of_Establishment) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *";
-module.exports = { insertUnitQ };
+const insertSoldierQ = "INSERT INTO soldier (Soldier_ID, Soldier_Name, Soldier_Rank, DOB, Gender, Unit_ID, Date_of_Enlistment, Date_of_Discharge, Regiment_Name, Phone, Blood_Type, Physical_Fitness_Scores) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *";
+const insertEquipmentQ = "INSERT INTO equipment ( Equipment_ID, Equipment_Name, Equipment_Type, Unit_ID, Quantity, Condition) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *";
+const insertCommanderQ = "INSERT INTO commander ( Commander_ID,Commander_Name,Commander_Rank,Unit_ID,Phone,Date_of_Commission,Awards) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *";
 
-
-const insertSoldierQ = "INSERT INTO soldier (soldier_id, soldier_name, soldier_rank, dob, gender, unit_id, date_of_enlistment, date_of_discharge, Regiment_Name, phone, blood_type, physical_fitness_scores) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *";
-module.exports = { insertSoldierQ };
+module.exports = {
+    insertUnitQ,
+    insertSoldierQ,
+    insertEquipmentQ,
+    insertCommanderQ
+};
