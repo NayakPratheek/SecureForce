c9ce833dfb259896e8c541761f6d21e7807a2bd8
Pratheek Nayak B K <128373632+NayakPratheek@users.noreply.github.com>
2024-03-23 23:05:29 +0530

    frontend changes

--- Frontend/src/Screen/Landing.jsx
+++ Frontend/src/Screen/Landing.jsx
@@ -29,6 +29,7 @@ function Landing() {
               </a>
               <a
                 href="http://localhost:3000/login"
+                target="_blank"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
                 View Database
@@ -84,6 +85,7 @@ function Landing() {
                 <li>
                   <a
                     href="https://www.joinindianarmy.nic.in/index.htm"
+                    target="_blank"
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     Join Indian Defence Forces
